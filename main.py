f721b72e4f6f3f967583b80a83cee83f566f2bd4
Tanvi Kachakayala <>
2021-05-26 22:23:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello stranger,let's create a dumb story")
+print()
+who= input("who is you favorite youtuber")
+where = input ("what is your favorite theme park")
+action = input ("what is your favorite activity to do")
+adjective = input ("what is your favorite smell")
+print("------- begin story--------")
+
+
+print("Once Opon A Time"+ who + "was in"+ where + "doing"+ action + "but everyone ran away  because they smelled like bbq chips")
